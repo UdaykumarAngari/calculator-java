4800dea10f7355f07dec7076c7b3c30561b32e2f
UdaykumarAngari <udaykumaruk5632@gmail.com>
2026-02-02 16:29:55 +0530

    updated pom.xml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculator calc = new Calculator();
+        System.out.println("Addition: " + calc.add(2, 3));
+        System.out.println("Subtraction: " + calc.subtract(3, 2));
     }
 }
